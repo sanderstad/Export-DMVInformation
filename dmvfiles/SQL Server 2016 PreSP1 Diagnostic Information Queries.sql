4f9905bab56e54116218a12bc30fc41f1d2ea83a
Sander Stad <sander@sqlstad.nl>
2017-04-20 08:39:18 +0200

    Update SQL Server 2016 PreSP1 Diagnostic Information Queries.sql

--- dmvfiles/SQL Server 2016 PreSP1 Diagnostic Information Queries.sql
+++ dmvfiles/SQL Server 2016 PreSP1 Diagnostic Information Queries.sql
@@ -418,8 +418,7 @@ physical_memory_kb/1024 AS [Physical Memory (MB)], committed_kb/1024 AS [Committ
 committed_target_kb/1024 AS [Committed Target Memory (MB)],
 max_workers_count AS [Max Workers Count], affinity_type_desc AS [Affinity Type], 
 sqlserver_start_time AS [SQL Server Start Time], virtual_machine_type_desc AS [Virtual Machine Type], 
-softnuma_configuration_desc AS [Soft NUMA Configuration],
-sql_memory_model_desc -- New in SQL Server 2016 SP1
+softnuma_configuration_desc AS [Soft NUMA Configuration]
 FROM sys.dm_os_sys_info WITH (NOLOCK) OPTION (RECOMPILE);
 ------
 
